eeabe624d7a1a42eacabb44c54a7f14a8e62949b
John Hattas <johnhattas@gmail.com>
2017-11-18 17:02:56 -0600

    tab editing

--- src/application/Main.java
+++ src/application/Main.java
@@ -13,6 +13,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.Separator;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import javafx.scene.control.TabPane.TabClosingPolicy;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
@@ -42,11 +43,26 @@ public class Main extends Application {
 		try {			
 			// main vertical layout
 			TabPane tabPane = new TabPane();
-			Tab tab = new Tab("Main");
-			BorderPane borderPane = new BorderPane();
-			//Add some in borderPane
-			tab.setContent(borderPane);
+			tabPane.setTabClosingPolicy(TabClosingPolicy.UNAVAILABLE);
+
 			
+			Tab homeTab = new Tab("Home");
+			BorderPane borderPane1 = new BorderPane();
+			borderPane1.getChildren().addAll(new Button("test"));
+			
+			Tab medTab = new Tab("Meds");
+			BorderPane borderPane2 = new BorderPane();
+			borderPane2.getChildren().addAll(new Button("test"));
+			
+			Tab logTab = new Tab("Log");
+			BorderPane borderPane3 = new BorderPane();
+			borderPane3.getChildren().addAll(new Button("test"));
+			
+			//Add some in borderPane
+			homeTab.setContent(borderPane1);
+			medTab.setContent(borderPane2);
+			logTab.setContent(borderPane3);
+			tabPane.getTabs().addAll(homeTab, medTab, logTab);
 
 	        Scene scene = new Scene(tabPane, 1280, 720);
 	        scene.getStylesheets().add("application/application.css");
