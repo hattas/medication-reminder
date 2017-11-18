c92a3974fab9d4297003b21a4a8dbc4786f963a6
John Hattas <johnhattas@gmail.com>
2017-11-18 17:43:14 -0600

    test home layout

--- src/application/HomeTab.java
+++ src/application/HomeTab.java
@@ -4,10 +4,5 @@ import javafx.scene.control.Tab;
 
 public class HomeTab extends Tab{
 	
-	protected HomeTab() {
-		Tab homeTab = new Tab();
-	}
-	
-	
-	
+
 }
--- src/application/Main.java
+++ src/application/Main.java
@@ -4,6 +4,7 @@ package application;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.stage.Stage;
@@ -15,6 +16,7 @@ import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.control.TabPane.TabClosingPolicy;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
@@ -47,21 +49,43 @@ public class Main extends Application {
 
 			
 			// Home Tab
-			Tab homeTab = new Tab();
-			// add content
-			homeTab.setContent();
+			Tab homeTab = new Tab("Home");
+			Label dateLabel = new Label("Saturday, November 18");
+			Label med1 = new Label("Advil");
+			GridPane.setConstraints(med1, 0, 0);
+			Label time1 = new Label("11:00");
+			GridPane.setConstraints(time1, 1, 0);
+			Label med2 = new Label("Tylenol");
+			GridPane.setConstraints(med2, 0, 1);
+			Label time2 = new Label("1:00");
+			GridPane.setConstraints(time2, 1, 1);
+			Label med3 = new Label("Ibuprofen");
+			GridPane.setConstraints(med3, 0, 2);
+			Label time3 = new Label("4:00");
+			GridPane.setConstraints(time3, 1, 2);
+			
+			GridPane grid = new GridPane();
+	        grid.setPadding(new Insets(10, 10, 10, 10));
+	        grid.setVgap(8);
+	        grid.setHgap(10);
+	        
+	        grid.getChildren().addAll(med1, time1, med2, time2, med3, time3);
+			
+			VBox homeVbox = new VBox();
+			homeVbox.getChildren().addAll(dateLabel, grid);
+			homeTab.setContent(homeVbox);
 
 			
 			// Medications Tab
-			Tab medTab = new Tab("Meds");
+			Tab medTab = new Tab("Medications");
 			// 
-			medTab.setContent();
+			//medTab.setContent();
 
 			
 			// History Log Tab
-			Tab logTab = new Tab("Log");
+			Tab logTab = new Tab("History Log");
 		
-			logTab.setContent();
+			//logTab.setContent();
 
 			
 			
