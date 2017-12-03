e061fa902befd3866435059d8c17c63653dcddbc
John Hattas <johnhattas@gmail.com>
2017-12-02 21:40:07 -0600

    populate table

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -1,50 +1,70 @@
 package application;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 /**
  * in fxml file:
  * fx:id refers to the object, like newButton
  * onAction refers to the method that is called
  */
 	
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
-public class Controller {
+public class Controller implements Initializable {
 
-    @FXML
-    private Label homeTimeLabel;
-    @FXML
-    private Label homeDateLabel;
-    @FXML
-    private Button newButton;
-    @FXML
-    private Button editButton;
-    @FXML
-    private Button deleteButton;
-    @FXML
-    private TableColumn<?, ?> colHomeTime;
-    @FXML
-    private TableColumn<?, ?> colHomeName;
-    @FXML
-    private TableColumn<?, ?> colHomeStatus;
-    @FXML
-    private TableColumn<?, ?> colMyDose;
-    @FXML
-    private TableColumn<?, ?> colMyName;
-    @FXML
-    private TableColumn<?, ?> colMyFrequency;
-    @FXML
-    private TableColumn<?, ?> colMyTime;
-    @FXML
-    private TableColumn<?, ?> colLogName;
-    @FXML
-    private TableColumn<?, ?> colLogTime;
-    @FXML
-    private TableColumn<?, ?> colLogDescription;
+    @FXML private Label homeTimeLabel;
+    @FXML private Label homeDateLabel;
+    
+    @FXML private Button newButton;
+    @FXML private Button editButton;
+    @FXML private Button deleteButton;
+    
+    @FXML private TableColumn<?, ?> colHomeTime;
+    @FXML private TableColumn<?, ?> colHomeName;
+    @FXML private TableColumn<?, ?> colHomeStatus;
+    @FXML private TableColumn<Medication, String> colMyDose;
+    @FXML private TableColumn<Medication, String> colMyName;
+    @FXML private TableColumn<Medication, String> colMyFrequency;
+    @FXML private TableColumn<Medication, String> colMyTime;
+    @FXML private TableColumn<?, ?> colLogName;
+    @FXML private TableColumn<?, ?> colLogTime;
+    @FXML private TableColumn<?, ?> colLogDescription;
+    
+    @FXML private TableView<Medication> medicationTable;
+    
+    
+    // initialize controller class
+    @Override
+	public void initialize(URL url, ResourceBundle resources) {
+		System.out.println("Initializing...");
+		colMyName.setCellValueFactory(new PropertyValueFactory<Medication, String>("name"));
+		colMyDose.setCellValueFactory(new PropertyValueFactory<Medication, String>("dose"));
+		colMyTime.setCellValueFactory(new PropertyValueFactory<Medication, String>("time"));
+		colMyFrequency.setCellValueFactory(new PropertyValueFactory<Medication, String>("frequency"));
+		
+		medicationTable.setItems(getMedications());
+		
+		System.out.println("Done initializing.");
+	}
+    
+    private ObservableList<Medication> getMedications() {
+    	ObservableList<Medication> medications = FXCollections.observableArrayList();
+    	medications.add(new Medication("Advil", "12:00", "Daily", "2"));
+    	medications.add(new Medication("Tylenol", "3:00", "Daily", "1"));
+    	medications.add(new Medication("Ibuprofen", "18:00", "Weekly", "3"));
+    	return medications;
+    }
 
+    
     @FXML
     private void newButtonClick() {
     	System.out.println("new");
--- src/application/Medication.java
+++ src/application/Medication.java
@@ -1,5 +1,7 @@
 package application;
 
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleStringProperty;
 
 /**
  * when user adds a medication, instance of this class will be created
@@ -8,35 +10,52 @@ package application;
 
 public class Medication {
 	
-	private String time, name, frequency;
-	private int amount;
+	private SimpleStringProperty name  = new SimpleStringProperty("");
+	private SimpleStringProperty time = new SimpleStringProperty("");
+	private SimpleStringProperty frequency = new SimpleStringProperty("");
+	private SimpleStringProperty dose = new SimpleStringProperty("");
 
 	public Medication() {
-		// this can be a constructor....
+		this("","","","");
+	}
+	
+	public Medication(String name, String time, String frequency, String dose) {
+		setName(name);
+		setTime(time);
+		setFrequency(frequency);
+		setDose(dose);
 	}
 	
 	// name of medication, string, "Advil"
-	public void setName(String name) {
-		this.name = name;}
 	public String getName() {
-		return name;}
+		return name.get();
+	}
+	public void setName(String name) {
+		this.name.set(name);
+	}
 	
 	// time of day to take med, type TBD, "14:30"
-	public void setTime(String time) {
-		this.time = time;}
 	public String getTime() {
-		return time;}
+		return time.get();
+	}
+	public void setTime(String time) {
+		this.time.set(time);
+	}
 	
-	// amount of med to be taken at time, integer, ex. 4
-	public void setAmount(int amount) {
-		this.amount = amount;}
-	public int getAmount() {
-		return amount;}
+	// dose of med to be taken at time, String, ex. 4
+	public String getDose() {
+		return dose.get();
+	}
+	public void setDose(String dose) {
+		this.dose.set(dose);
+	}
 	
 	// how often med is taken, String, "Weekly" "Daily" etc.
-	public void setFrequency(String frequency) {
-		this.frequency = frequency;}
 	public String getFrequency() {
-		return frequency;}
+		return frequency.get();
+	}
+	public void setFrequency(String frequency) {
+		this.frequency.set(frequency);
+	}
 	
 }
