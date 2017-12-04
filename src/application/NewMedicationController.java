89f9af9f10a84909b1b924cb1445fe600ddf92c7
John Hattas <johnhattas@gmail.com>
2017-12-03 21:28:49 -0600

    add please fill out all fields message for add popup

--- src/application/NewMedicationController.java
+++ src/application/NewMedicationController.java
@@ -1,13 +1,19 @@
 package application;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import listobjects.Medication;
 
-public class NewMedicationController {
+public class NewMedicationController implements Initializable{
 	
+	@FXML private Label label;
 	@FXML private Button addButton;
 	@FXML private Button cancelButton;
 	@FXML private TextField addNameField;
@@ -15,6 +21,11 @@ public class NewMedicationController {
 	@FXML private TextField addDoseField;
 	@FXML private TextField addTimeField;
 	boolean addButtonClicked = false;
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		label.setVisible(false);
+	}
 	
     public Medication getMedication() {
         return new Medication(addNameField.getText(),addTimeField.getText(),addFrequencyField.getText(),addDoseField.getText());
@@ -31,6 +42,9 @@ public class NewMedicationController {
 			Stage stage = (Stage) cancelButton.getScene().getWindow();
 			stage.close();
 		}
+		else {
+			label.setVisible(true);
+		}
 	}
 	
 	/**
@@ -42,4 +56,6 @@ public class NewMedicationController {
 		stage.close();
 		addButtonClicked = false;
 	}
+
+	
 }
